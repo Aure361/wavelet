72d5328e9113b7e4c06434534c318278d6a3f197
Aure361 <114559967+Aure361@users.noreply.github.com>
2022-10-13 18:05:55 -0700

    server update

--- NumberServer.java
+++ NumberServer.java
@@ -8,7 +8,7 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
+            return String.format("Number: %d", num);//what is the purpose of %d and %s
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
@@ -17,7 +17,7 @@ class Handler implements URLHandler {
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
+                    num += Integer.parseInt(parameters[1]);//parseint converts a number from the link to an int
                     return String.format("Number increased by %s! It's now %d", parameters[1], num);
                 }
             }
--- SearchEngine.java
+++ SearchEngine.java
@@ -1,24 +1,23 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    String s = "";
-
+    ArrayList<String> stringList = new ArrayList<>();
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("String: %d", s);
-        } else if (url.getPath().equals("/increment")) {
-            s += "test";
-            return String.format("Number incremented!");
+            return String.format("Stringlist: ", stringList);
+        } else if (url.getPath().equals("/update")) {
+            return String.format("Stringlist is now: ", stringList);
         } else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    stringList.add(parameters[1]);
+                    return String.format("Stringlist is now:" + stringList);
                 }
             }
             return "404 Not Found!";
@@ -26,7 +25,7 @@ class Handler implements URLHandler {
     }
 }
 
-class SearchEngine {
+class NumberServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
