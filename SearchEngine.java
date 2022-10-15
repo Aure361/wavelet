f4800fd5062617846f655c489a6fb2eb8786f849
Aure361 <114559967+Aure361@users.noreply.github.com>
2022-10-14 18:20:25 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -8,16 +8,16 @@ class Handler implements URLHandler {
     ArrayList<String> stringList = new ArrayList<>();
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Stringlist: ", stringList);
+            return String.format("Stringlist: ");
         } else if (url.getPath().equals("/Stringlist")) {
-            return String.format("Stringlist is now: ", stringList);
+            return String.format("Stringlist is now: " + stringList);
         } else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
                     stringList.add(parameters[1]);
-                    return String.format("Stringlist is now:" + stringList);
+                    return String.format("Has been added to StringList: " + parameters[1]);
                 }
             }
             return "404 Not Found!";
